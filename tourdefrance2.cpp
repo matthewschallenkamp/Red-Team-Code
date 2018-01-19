972d895bdf519e63726b64ee11e5f8b977ce3af8
Matthew Schallenkamp <matthew.schallenkamp@gmail.com>
2018-01-18 19:39:57 -0800

    Update tourdefrance2.cpp

--- tourdefrance2.cpp
+++ tourdefrance2.cpp
@@ -11,35 +11,35 @@ using namespace std;
 typedef unsigned long long ull;
 typedef long long ll;
 
+//these stay the same per dfs, so globals are simple.
 vector<vector<pair<int, int> > > edges;
 ll all;
 
 int dfs_len(int current = 0, ll visited = 0)
 {
-  // cerr << current << " " << visited << endl;
+  //guaranteed max is 37 * 10000 = 370000, and that there is a solution, so this is safe
   int best = 10000000;
   for(auto p : edges[current])
   {
+    //we always start and end at node 0. check if this is a full path
     if(p.first == 0)
     {
       if((visited | 1LL) == all)
       {
-        //we are finished, this is a path
+        //we are finished, this is a valid path, update our best
         best = min(best, p.second);
       }
-      //else this is not a full path, ignore
+      //else this is not a full path (cannot repeat nodes), ignore it
     }
     else
     {
-      //there is more to this path, check we can visit this spot
+      //there is more to this path, check we can visit this spot and update our best
       if(! (visited & (1LL << p.first)))
         best = min(best, p.second + dfs_len(p.first, visited | (1LL << p.first)));
-    }
-    
+    }    
   }
   
   return best;
-  
 }
 
 int main()
@@ -48,9 +48,10 @@ int main()
   cin >> tc;
   while(tc--)
   {
-    // cerr << "\n\n\n\n";
+    // nodes, edges, from, to, cost
     int n, m, f, t, c;
     cin >> n >> m;
+    // 1's for each node to visit
     all = (1LL << n) - 1;
 
     edges.clear();
@@ -58,6 +59,8 @@ int main()
     while(m--)
     {
       cin >> f >> t >> c;
+      // the first index is the from node, second index doesnt matter
+      // first item in the pair is the to node, and the second item is the cost
       edges[f].push_back(make_pair(t, c));
     }
 
